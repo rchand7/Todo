a1bae599346bc56e6b98f1ff21acdfb5a46736b7
rchand7 <150305815+rchand7@users.noreply.github.com>
2025-09-01 18:39:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Wrap in quotes
+    origin: "https://todo-12-bsj5.onrender.com", // Wrap in quotes
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Use array instead of string
     allowedHeaders: ["Content-Type", "Authorization"],
